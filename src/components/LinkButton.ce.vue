8edf8883deb91a51b84755f14760e56735618ad3
Dennis Kleber <dennis_kleber@mailbox.org>
2024-08-16 10:59:27 +0200

    link-button

---
+++ src/components/LinkButton.ce.vue
@@ -0,0 +1,58 @@
+<template>
+    <Button :class="css" @click="submit">{{ label }}</Button>
+</template>
+<script setup lang="ts">
+    import { ColorType } from '@/types/Default';
+    import { Button } from '@vivaconagua/vueca';
+    import { computed } from 'vue';
+    const props = defineProps<{
+        label: string;
+        to: string;
+        utm_source: string | undefined;
+        utm_medium: string | undefined;
+        utm_campaign: string | undefined;
+        color: string | undefined;
+    }>();
+    const submit = () => {
+        window.location.href = props.to + campaign();
+    };
+    const campaign = () => {
+        if (!props.utm_campaign && !props.utm_medium && !props.utm_source) {
+            return '';
+        } else {
+            const query = [];
+            if (props.utm_source) {
+                query.push('utm_source=' + props.utm_source);
+            }
+            if (props.utm_medium) {
+                query.push('utm_medium=' + props.utm_medium);
+            }
+            if (props.utm_campaign) {
+                query.push('utm_campaign=' + props.utm_campaign);
+            }
+            let queryString = '?';
+            query.forEach((e) => {
+                queryString = queryString + e + '&';
+            });
+            queryString = queryString.substring(0, queryString.length - 1);
+            return queryString;
+        }
+    };
+    const css = computed(() => {
+        if (props.color != undefined) {
+            if (props.color == ColorType.default) {
+                return '';
+            }
+            if (props.color == ColorType.orange) {
+                return 'bg-orange';
+            }
+            if (props.color == ColorType.dark) {
+                return 'bg-dark';
+            }
+        }
+        return '';
+    });
+</script>
+<style lang="scss">
+    @import 'Button.scss';
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { defineCustomElement } from 'vue';
 import copyButton from './components/CopyButton.ce.vue';
+import linkButton from './components/LinkButton.ce.vue';
+
 const copyButtonComponent = defineCustomElement(copyButton);
+const linkButtonComponent = defineCustomElement(linkButton);
 
 customElements.define('copy-button', copyButtonComponent);
+customElements.define('link-button', linkButtonComponent);
