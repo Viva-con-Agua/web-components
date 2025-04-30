e8bb8bbae5963c5285955c8e6a356b450f37c476
Dennis Kleber <dennis_kleber@mailbox.org>
2025-04-30 10:12:45 +0200

    add pdf download

---
+++ src/components/DownloadContainer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="downloadContainer">
+        <slot></slot>
+    </div>
+</template>
+<script setup lang="ts">
+    // PDF URL, die hier festgelegt wird (kann auch als Prop übergeben werden)
+    //const pdfUrl: string = props.downloadLink;
+
+    // Referenz auf das Canvas-Element
+</script>
+<style lang="scss">
+    .downloadContainer {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 0.5em;
+        align-content: center;
+        justify-content: center;
+    }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { defineCustomElement } from 'vue';
 import copyButton from './components/CopyButton.vue';
-import linkButton from './components/LinkButton.vue';
+import downloadContainer from './components/DownloadContainer.vue';
+import downloadPDF from './components/DownloadPDF.vue';
 import formWrapper from './components/FormWrapper.vue';
+import linkButton from './components/LinkButton.vue';
+
 const copyButtonComponent = defineCustomElement(copyButton);
 const linkButtonComponent = defineCustomElement(linkButton);
 const formWrapperComponent = defineCustomElement(formWrapper);
+const downloadPDFComponent = defineCustomElement(downloadPDF);
+const downloadContainerComponent = defineCustomElement(downloadContainer);
 
 customElements.define('copy-button', copyButtonComponent);
 customElements.define('link-button', linkButtonComponent);
 customElements.define('form-wrapper', formWrapperComponent);
+customElements.define('download-pdf', downloadPDFComponent);
+customElements.define('download-container', downloadContainerComponent);
